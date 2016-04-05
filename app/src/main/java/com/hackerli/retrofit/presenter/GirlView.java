260c4d5dca95ee833c9552a23117460b5fe1162f
Jianxin <838171740@qq.com>
2016-04-05 19:52:20 +0800

    try MVP

---
+++ app/src/main/java/com/hackerli/retrofit/presenter/GirlView.java
@@ -0,0 +1,14 @@
+package com.hackerli.retrofit.presenter;
+
+import android.support.annotation.Nullable;
+
+import com.hackerli.retrofit.data.entity.Girl;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2016/4/5.
+ */
+public interface GirlView {
+    public void loadMore(@Nullable List<Girl> girls);
+}
