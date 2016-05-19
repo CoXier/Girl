9b81ad01267928c1ffdd4941ba54d2c5cb657a63
CoXier <TmWorkCC@yahoo.com>
2016-05-19 21:16:54 +0800

    avoid cold start

---
+++ app/src/main/java/com/hackerli/retrofit/entry/EntryActivity.java
@@ -0,0 +1,16 @@
+package com.hackerli.retrofit.entry;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.hackerli.retrofit.R;
+
+public class EntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entry);
+
+    }
+}
