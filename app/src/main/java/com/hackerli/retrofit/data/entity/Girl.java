8b5159743b21ff1cac404bf21f8c8186803d1fdf
CoXier <TmWorkCC@yahoo.com>
2016-05-19 20:14:29 +0800

    cut down property

--- app/src/main/java/com/hackerli/retrofit/data/entity/Girl.java
+++ app/src/main/java/com/hackerli/retrofit/data/entity/Girl.java
@@ -1,7 +1,5 @@
 package com.hackerli.retrofit.data.entity;
 
-import com.google.gson.annotations.SerializedName;
-
 /**
  * Created by Administrator on 2016/3/20.
  */
@@ -19,42 +17,11 @@ public class Girl {
      * who : 张涵宇
      */
 
-    @SerializedName("_id")
-    private String id;
-    @SerializedName("_ns")
-    private String ns;
-    private String createdAt;
     private String desc;
-    private String publishedAt;
-    private String source;
-    private String type;
     private String url;
-    private boolean used;
-    private String who;
 
-    public String getId() {
-        return id;
-    }
 
-    public void setId(String id) {
-        this.id = id;
-    }
 
-    public String getNs() {
-        return ns;
-    }
-
-    public void setNs(String ns) {
-        this.ns = ns;
-    }
-
-    public String getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(String createdAt) {
-        this.createdAt = createdAt;
-    }
 
     public String getDesc() {
         return desc;
@@ -64,51 +31,7 @@ public class Girl {
         this.desc = desc;
     }
 
-    public String getPublishedAt() {
-        return publishedAt;
-    }
-
-    public void setPublishedAt(String publishedAt) {
-        this.publishedAt = publishedAt;
-    }
-
-    public String getSource() {
-        return source;
-    }
-
-    public void setSource(String source) {
-        this.source = source;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
     public String getUrl() {
         return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public boolean isUsed() {
-        return used;
-    }
-
-    public void setUsed(boolean used) {
-        this.used = used;
-    }
-
-    public String getWho() {
-        return who;
-    }
-
-    public void setWho(String who) {
-        this.who = who;
     }
 }
