f1d4d7d85af2f080d0b807ec321c6100b5bb84da
CoXier <justcoxier@gmail.com>
2016-12-22 20:48:47 +0800

    just put github's id and key into the interface

--- app/src/main/java/com/hackerli/retrofit/api/GitHubService.java
+++ app/src/main/java/com/hackerli/retrofit/api/GitHubService.java
@@ -11,7 +11,9 @@ import rx.Observable;
  * Created by CoXier on 2016/5/4.
  */
 public interface GitHubService {
+    String clientID = "b78af009a1b1cfe46317";
+    String clientSecret = "6d96f809338d479ed86614dd09983195119d338c";
 
     @GET("users/{user}")
-    public Observable<GitUser> getAvatar(@Path("user")String user, @Query("client_id")String id,@Query("client_secret")String secret);
+    Observable<GitUser> getAvatar(@Path("user")String user, @Query("client_id")String id,@Query("client_secret")String secret);
 }
---
+++ app/src/main/java/com/hackerli/retrofit/ui/SearchWindow.java
@@ -0,0 +1,8 @@
+package com.hackerli.retrofit.ui;
+
+/**
+ * Created by CoXier on 16-12-22.
+ */
+
+public class SearchWindow {
+}
