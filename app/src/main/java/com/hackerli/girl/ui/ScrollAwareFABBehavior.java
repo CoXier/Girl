2f1311dee972492263b507c5bde0c371a110edc3
lijianxin <lijianxin@bytedance.com>
2017-07-15 21:12:34 +0800

    fix #3
    Change-Id: Ibfad9d5a7015d98ee0d0831a28d0d72a1095a0c9

--- app/src/main/java/com/hackerli/girl/ui/ScrollAwareFABBehavior.java
+++ app/src/main/java/com/hackerli/girl/ui/ScrollAwareFABBehavior.java
@@ -1,7 +1,6 @@
 package com.hackerli.girl.ui;
 
 import android.content.Context;
-import android.os.Build;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.view.ViewCompat;
@@ -9,8 +8,6 @@ import android.support.v4.view.ViewPropertyAnimatorListener;
 import android.support.v4.view.animation.FastOutSlowInInterpolator;
 import android.util.AttributeSet;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.view.animation.Interpolator;
 
 /**
@@ -18,7 +15,6 @@ import android.view.animation.Interpolator;
  * Meizhi is under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
  */
 public class ScrollAwareFABBehavior extends FloatingActionButton.Behavior {
     private static final Interpolator INTERPOLATOR = new FastOutSlowInInterpolator();
@@ -46,11 +42,8 @@ public class ScrollAwareFABBehavior extends FloatingActionButton.Behavior {
                 dxUnconsumed, dyUnconsumed);
         if (dyConsumed > 0 && !this.mIsAnimatingOut &&
                 child.getVisibility() == View.VISIBLE) {
-            // User scrolled down and the FAB is currently visible -> hide the FAB
             animateOut(child);
-        }
-        else if (dyConsumed < 0 && child.getVisibility() != View.VISIBLE) {
-            // User scrolled up and the FAB is currently not visible -> show the FAB
+        } else if (dyConsumed < 0 && child.getVisibility() != View.VISIBLE) {
             animateIn(child);
         }
     }
@@ -58,75 +51,44 @@ public class ScrollAwareFABBehavior extends FloatingActionButton.Behavior {
 
     // Same animation that FloatingActionButton.Behavior uses to hide the FAB when the AppBarLayout exits
     private void animateOut(final FloatingActionButton button) {
-        if (Build.VERSION.SDK_INT >= 14) {
-            ViewCompat.animate(button)
-                    .scaleX(0.0F)
-                    .scaleY(0.0F)
-                    .alpha(0.0F)
-                    .setInterpolator(INTERPOLATOR)
-                    .withLayer()
-                    .setListener(new ViewPropertyAnimatorListener() {
-                        public void onAnimationStart(View view) {
-                            ScrollAwareFABBehavior.this.mIsAnimatingOut = true;
-                        }
+        ViewCompat.animate(button)
+                .scaleX(0.0F)
+                .scaleY(0.0F)
+                .alpha(0.0F)
+                .setInterpolator(INTERPOLATOR)
+                .withLayer()
+                .setListener(new ViewPropertyAnimatorListener() {
+                    public void onAnimationStart(View view) {
+                        ScrollAwareFABBehavior.this.mIsAnimatingOut = true;
+                    }
 
 
-                        public void onAnimationCancel(View view) {
-                            ScrollAwareFABBehavior.this.mIsAnimatingOut = false;
-                        }
+                    public void onAnimationCancel(View view) {
+                        ScrollAwareFABBehavior.this.mIsAnimatingOut = false;
+                    }
 
 
-                        public void onAnimationEnd(View view) {
-                            ScrollAwareFABBehavior.this.mIsAnimatingOut = false;
-                            view.setVisibility(View.GONE);
-                        }
-                    })
-                    .start();
-        }
-        else {
-            Animation anim = AnimationUtils.loadAnimation(button.getContext(),
-                    android.support.design.R.anim.design_fab_out);
-            anim.setInterpolator(INTERPOLATOR);
-            anim.setDuration(200L);
-            anim.setAnimationListener(new Animation.AnimationListener() {
-                public void onAnimationStart(Animation animation) {
-                    ScrollAwareFABBehavior.this.mIsAnimatingOut = true;
-                }
+                    public void onAnimationEnd(View view) {
+                        ScrollAwareFABBehavior.this.mIsAnimatingOut = false;
+                        view.setVisibility(View.INVISIBLE);
+                    }
+                })
+                .start();
 
-
-                public void onAnimationEnd(Animation animation) {
-                    ScrollAwareFABBehavior.this.mIsAnimatingOut = false;
-                    button.setVisibility(View.GONE);
-                }
-
-
-                @Override public void onAnimationRepeat(final Animation animation) {
-                }
-            });
-            button.startAnimation(anim);
-        }
     }
 
 
     // Same animation that FloatingActionButton.Behavior uses to show the FAB when the AppBarLayout enters
     private void animateIn(FloatingActionButton button) {
         button.setVisibility(View.VISIBLE);
-        if (Build.VERSION.SDK_INT >= 14) {
-            ViewCompat.animate(button)
-                    .scaleX(1.0F)
-                    .scaleY(1.0F)
-                    .alpha(1.0F)
-                    .setInterpolator(INTERPOLATOR)
-                    .withLayer()
-                    .setListener(null)
-                    .start();
-        }
-        else {
-            Animation anim = AnimationUtils.loadAnimation(button.getContext(),
-                    android.support.design.R.anim.design_fab_in);
-            anim.setDuration(200L);
-            anim.setInterpolator(INTERPOLATOR);
-            button.startAnimation(anim);
-        }
+        ViewCompat.animate(button)
+                .scaleX(1.0F)
+                .scaleY(1.0F)
+                .alpha(1.0F)
+                .setInterpolator(INTERPOLATOR)
+                .withLayer()
+                .setListener(null)
+                .start();
+
     }
 }
